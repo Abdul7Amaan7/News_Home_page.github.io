994ff6a9fd085c39ddac76acddc1bcefc9cbc38b
Abdul Amaan <99074207+Abdul7Amaan7@users.noreply.github.com>
2022-10-29 21:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu_click = () => {
+  let a = document.getElementById("menu-button");
+  a.classList.toggle("cross");
+  document.getElementById("remove").classList.toggle("ds-remove");
+  console.log("sab thik he ");
+};
